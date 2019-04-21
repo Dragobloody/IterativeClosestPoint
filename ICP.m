55a118addd7959ef138c83d7296425e182bd48a6
Unknown <grama.dragos.ioan@gmail.com>
2019-04-21 23:13:16 +0200

    Getting used to data

---
+++ ICP.m
@@ -0,0 +1,12 @@
+function [R, t, error, newA1] = ICP(source, target, point_sample_method)
+    A2 = target
+    switch(point_sample_method)
+        case 'all'            
+            A1 = source
+            
+        
+    end
+    
+    
+end
+
---
+++ main.m
@@ -0,0 +1,7 @@
+
+test = readPcd('Data/data/0000000000.pcd');
+
+%%
+
+fscatter3(test(1,:),test(2,:),test(3,:),test(4,:));
+
