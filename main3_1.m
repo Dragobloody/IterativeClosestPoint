920e04730175929f6e763935679fa7d4553b5227
Unknown <grama.dragos.ioan@gmail.com>
2019-04-23 14:35:28 +0200

    Small update on main3.1

--- main3_1.m
+++ main3_1.m
@@ -4,39 +4,42 @@ path = 'Data/data/' ;
 
 %% 3.1
 % Initialize matrices outside the loop 
-target_file = strcat(path, num2str(0, '%010d'), '.pcd');
-target = readPcd(target_file);
-[target_mat, target_rgb] = preprocessPointCloud(target, 1);
-
-R_previous = eye(3) ;
-t_previous = zeros(3,1) ;
+source_file = strcat(path, num2str(0, '%010d'), '.pcd');
+source = readPcd(source_file);
+[source_mat, source_rgb] = preprocessPointCloud(source, 1);
 
 step = 1 ; % step is 1 for a) and 2, 4, 10 for b)
-merged = target_mat ;
+merged = source_mat ;
 
-for j = [0:step:10]
+for j = [0:step:(99-step)]
     j
     source_file = strcat(path, num2str(j, '%010d'), '.pcd');
     source_normals_file = strcat(path, num2str(j, '%010d'), '_normal.pcd');
+    target_file = strcat(path, num2str(j+step, '%010d'), '.pcd');
     
     source = readPcd(source_file) ;
     source_normals = readPcd(source_normals_file) ;
+    target = readPcd(target_file);
     
     [source_mat, source_normals_mat] = preprocessNormals(source, source_normals) ;
+    [target_mat, target_rgb] = preprocessPointCloud(target, 1);
     [R, t, error, transformed] = ICP(source_mat, source_normals_mat, target_mat, 'informative', 100, 0) ;
     
     % transformed is from source->target, we want to get to the original
     % camera pose:
-    transformed = R_previous * transformed + t_previous ; 
+    merged = R*merged + t;
+    %transformed = R_previous * transformed + t_previous ; 
     
-    merged = [merged transformed] ;
+    merged = [merged target_mat] ;
     
     % Keep track of all transf. to get back to the original camera pose
-    R_previous = R_previous * R;
-    t_previous = t_previous + t;
+    %R_previous = R_previous * R;
+    %t_previous = t_previous + t;
     
-    target_mat = source_mat ;
+    %target_mat = source_mat ;
 end
+
+%%
 
 fscatter3(merged(1, :), merged(2, :), merged(3, :), merged(3, :));
 
