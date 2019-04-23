d0c5c6fb86d26ada81db74d5a7c169b85977bdd6
Unknown <grama.dragos.ioan@gmail.com>
2019-04-23 18:57:55 +0200

    Improved speed for main3.2

--- main3_1.m
+++ main3_1.m
@@ -8,39 +8,52 @@ source_file = strcat(path, num2str(0, '%010d'), '.pcd');
 source = readPcd(source_file);
 [source_mat, source_rgb] = preprocessPointCloud(source, 1);
 
-step = 1 ; % step is 1 for a) and 2, 4, 10 for b)
+step = 2 ; % step is 1 for a) and 2, 4, 10 for b)
 merged = source_mat ;
 
-for j = [0:step:(99-step)]
+R_previous = eye(3);
+t_previous = zeros(3,1);
+
+for j = [0:step:(20-step)]
     j
-    source_file = strcat(path, num2str(j, '%010d'), '.pcd');
-    source_normals_file = strcat(path, num2str(j, '%010d'), '_normal.pcd');
-    target_file = strcat(path, num2str(j+step, '%010d'), '.pcd');
+    source_file = strcat(path, num2str(j+step, '%010d'), '.pcd');
+    source_normals_file = strcat(path, num2str(j+step, '%010d'), '_normal.pcd');
+    target_file = strcat(path, num2str(j, '%010d'), '.pcd');
+    target_normals_file = strcat(path, num2str(j, '%010d'), '_normal.pcd');
     
     source = readPcd(source_file) ;
     source_normals = readPcd(source_normals_file) ;
     target = readPcd(target_file);
+    target_normals = readPcd(target_normals_file) ;
     
     [source_mat, source_normals_mat] = preprocessNormals(source, source_normals) ;
-    [target_mat, target_rgb] = preprocessPointCloud(target, 1);
+    [target_mat, target_normals_mat] = preprocessNormals(target, target_normals) ;    
     [R, t, error, transformed] = ICP(source_mat, source_normals_mat, target_mat, 'informative', 100, 0) ;
     
     % transformed is from source->target, we want to get to the original
     % camera pose:
-    merged = R*merged + t;
-    %transformed = R_previous * transformed + t_previous ; 
-    
-    merged = [merged target_mat] ;
+    transformed = R_previous*transformed + t_previous;
+   
+    merged = [merged transformed] ;
     
     % Keep track of all transf. to get back to the original camera pose
-    %R_previous = R_previous * R;
-    %t_previous = t_previous + t;
+    R_previous = R_previous * R;
+    t_previous = t_previous + R_previous*t;
     
     %target_mat = source_mat ;
 end
 
 %%
-
 fscatter3(merged(1, :), merged(2, :), merged(3, :), merged(3, :));
-
-
+%%
+fscatter3(source_mat(1, :), source_mat(2, :), source_mat(3, :), source_mat(3, :));
+figure()
+fscatter3(target_mat(1, :), target_mat(2, :), target_mat(3, :), target_mat(3, :));
+figure()
+fscatter3(transformed(1, :), transformed(2, :), transformed(3, :), transformed(3, :));
+%%
+fscatter3(transformed(1, :), transformed(2, :), transformed(3, :), transformed(3, :));
+figure()
+fscatter3(transformed1(1, :), transformed1(2, :), transformed1(3, :), transformed1(3, :));
+%%
+save('merged_pointclouds/merged_3_1_a.mat', 'merged');
--- main3_2.m
+++ main3_2.m
@@ -5,28 +5,33 @@ path = 'Data/data/' ;
 %% 3.2
 % Initialize matrices outside the loop 
 
-target_file = strcat(path, num2str(1, '%010d'), '.pcd');
-target = readPcd(target_file);
-[target_mat, target_rgb] = preprocessPointCloud(target, 1);
+source_file = strcat(path, num2str(0, '%010d'), '.pcd');
+source_normals_file = strcat(path, num2str(0, '%010d'), '_normal.pcd');    
+source = readPcd(source_file);
+source_normals = readPcd(source_normals_file) ;
+[source_mat, source_normals_mat] = preprocessNormals(source, source_normals) ;
 
-merged_b = target_mat;
-step = 10 ;
-for j = [0:step:99]
-   
-    source_file = strcat(path, num2str(j, '%010d'), '.pcd');
-    source_normals_file = strcat(path, num2str(j, '%010d'), '_normal.pcd');
+merged_source = source_mat;
+merged_source_normals = source_normals_mat;
+step = 1 ;
+for j = [1:step:10]
+    j
+    target_file = strcat(path, num2str(j, '%010d'), '.pcd');
+    target_normals_file = strcat(path, num2str(j, '%010d'), '_normal.pcd');
     
-    source = readPcd(source_file) ;
-    source_normals = readPcd(source_normals_file) ;
+    target = readPcd(target_file) ;
+    target_normals = readPcd(target_normals_file) ;
     
     tic
-    [source_mat, source_normals_mat] = preprocessNormals(source, source_normals) ;
-    [R, t, error, transformed] = ICP(source_mat, source_normals_mat, merged_b, 'informative', 100, 0) ;    
+    [target_mat, target_normals_mat] = preprocessNormals(target, target_normals) ;
+    [R, t, error, transformed] = ICP(merged_source, merged_source_normals, target_mat, 'informative', 100, 0) ;    
     
     % The new target will be the merged cloud points, which gives more
     % robustness to the reconstruction
-    merged_b = [merged_b transformed] ;
+    merged_source = R*merged_source + t;
+    merged_source_normals = R*merged_source_normals + t;
+    merged_source = [merged_source target_mat] ;
     toc
 end
-
-fscatter3(merged_b(1, :), merged_b(2, :), merged_b(3, :), merged_b(3, :));
+%%
+fscatter3(merged_source(1, :), merged_source(2, :), merged_source(3, :), merged_source(3, :));
